d99a240e4e5552868a12e923fbeab46b9b96b962
deva panda <66827166+devapanda@users.noreply.github.com>
2020-11-24 19:30:41 +0530

    upload

---
+++ js/slider-index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+	$('#js-main-slider').pogoSlider({
+		autoplay: false,
+		autoplayTimeout: 5000,
+		displayProgess: false,
+		preserveTargetSize: false,
+		targetWidth: 1000,
+		targetHeight: 300,
+		responsive: false
+	}).data('plugin_pogoSlider');
+
+	var transitionDemoOpts = {
+		displayProgess: false,
+		generateNav: false,
+		generateButtons: false
+	}
+
+});
